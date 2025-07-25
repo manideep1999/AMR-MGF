9e83268e883f383a329512732fab6c468cc5d010
Mani Deep Cherukuri <manideep2705@gmail.com>
2025-07-24 23:16:12 -0500

    HF changes added; Restaurant 87.1 accuracy logs

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # * laptop
-#  python ./train.py --model_name EHFB --dataset laptop --seed 1000 --bert_lr 2e-5 --batch_size 8 --num_epoch 15 --bert_dim 768 --max_length 100 --vocab_dir ./dataset/Laptops_corenlp --gamma 1 --theta 0.12\
-#                     --max_num_spans 6 --dep_layers 3 --sem_layers 6 --fusion_condition ResEMFH 
+ python ./train.py --model_name EHFB --dataset laptop --seed 42 --bert_lr 2e-5 --batch_size 32 --num_epoch 15 --bert_dim 768 --max_length 100 --vocab_dir ./dataset/Restaurants_corenlp_AMRBART --gamma 1 --theta 0.5\
+                    --max_num_spans 6 --dep_layers 3 --sem_layers 6 --amr_layers 6 --fusion_condition ResEMFH 
 
 # * restaurants
- python ./train.py --model_name EHFB --dataset restaurant --seed 1000 --bert_lr 2e-5 --batch_size 16 --num_epoch 20 --vocab_dir ./dataset/Restaurants_corenlp_AMRBART --bert_dim 768 --max_length 100 \
-                    --max_num_spans 3 --dep_layers 3 --sem_layers 9 --amr_layers 9 --fusion_condition ResEMFH --gamma 1 --theta 0.12
+#  python ./train.py --model_name EHFB --dataset restaurant --seed 42 --bert_lr 2e-5 --batch_size 32 --num_epoch 20 --vocab_dir ./dataset/Restaurants_corenlp_AMRBART --bert_dim 768 --max_length 100 \
+#                     --max_num_spans 6 --dep_layers 6 --sem_layers 9 --amr_layers 9 --fusion_condition HF --gamma 1 --theta 0.5 
 
 # * twitter
 # python ./train.py --model_name EHFB --dataset twitter --seed 1000 --bert_lr 2e-5 --batch_size 16 --num_epoch 20 --vocab_dir ./dataset/Tweets_corenlp --bert_dim 768 --max_length 100 \
